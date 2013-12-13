f9da578df5772ed8524947555223b0aa7d2ef499
Ryan John <ryan.john@kotikan.com>
2013-12-13 11:39:43 +0000

    Removed unused buttons and added empy MapView

--- src/com/orbotix/sample/locator/LocatorActivity.java
+++ src/com/orbotix/sample/locator/LocatorActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -16,6 +17,7 @@ import orbotix.sphero.LocatorListener;
 import orbotix.sphero.Sphero;
 import orbotix.view.connection.SpheroConnectionView;
 
+import java.util.Map;
 import java.util.Random;
 
 public class LocatorActivity extends Activity {
@@ -41,6 +43,7 @@ public class LocatorActivity extends Activity {
     private float currentAngle = 0;
 
     private Random randomGenerator;
+    private Map locationMap;
 
     private LocatorListener mLocatorListener = new LocatorListener() {
         @Override
@@ -52,10 +55,6 @@ public class LocatorActivity extends Activity {
                 velocityx = locatorData.getVelocityX();
                 velocityy = locatorData.getVelocityY();
 
-                ((TextView) findViewById(R.id.txt_locator_x)).setText(positionx + " cm");
-                ((TextView) findViewById(R.id.txt_locator_y)).setText(positiony + " cm");
-                ((TextView) findViewById(R.id.txt_locator_vx)).setText(velocityx + " cm/s");
-                ((TextView) findViewById(R.id.txt_locator_vy)).setText(velocityy + " cm/s");
             }
         }
     };
@@ -65,14 +64,15 @@ public class LocatorActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        findViewById(R.id.back_layout).requestFocus();
 
         randomGenerator = new Random();
         currentAngle = randomGenerator.nextInt(359);
 
+        Button startButton = (Button) findViewById(R.id.start);
+        Button stopButton = (Button) findViewById(R.id.stop);
 
         mSpheroConnectionView = (SpheroConnectionView) findViewById(R.id.sphero_connection_view);
-//        mSpheroConnectionView.setSingleSpheroMode(true);
+        mSpheroConnectionView.setSingleSpheroMode(true);
         // Set the connection event listener 
         mSpheroConnectionView.addConnectionListener(new ConnectionListener() {
             @Override
@@ -127,63 +127,12 @@ public class LocatorActivity extends Activity {
         }
     }
 
-    /**
-     * When the user clicks the configure button, it calls this function
-     *
-     * @param v
-     */
-    public void configurePressed(View v) {
-
-        if (mRobot == null) return;
-
-        int newX = 0;   // The locator's current X position value will be set to this value
-        int newY = 0;   // The locator's current Y position value will be set to this value
-        int newYaw = 0; // The yaw value you set this to, will represent facing down the +y_axis
-
-        // Try parsing the integer values from the edit text boxes, if not, use zeros
-        try {
-            newX = Integer.parseInt(((EditText) findViewById(R.id.edit_new_x)).getText().toString());
-        } catch (NumberFormatException e) {
-        }
-
-        try {
-            newY = Integer.parseInt(((EditText) findViewById(R.id.edit_new_y)).getText().toString());
-        } catch (NumberFormatException e) {
-        }
-
-        try {
-            newYaw = Integer.parseInt(((EditText) findViewById(R.id.edit_new_yaw)).getText().toString());
-        } catch (NumberFormatException e) {
-        }
-
-        // Flag will be true if the check box is clicked, false if it is not
-        // When the flag is off (default behavior) the x, y locator grid is rotated with the calibration
-        // When the flag is on the x, y locator grid is fixed and Sphero simply calibrates within it
-        int flag = ((CheckBox) findViewById(R.id.checkbox_flag)).isChecked() ?
-                ConfigureLocatorCommand.ROTATE_WITH_CALIBRATE_FLAG_ON :
-                ConfigureLocatorCommand.ROTATE_WITH_CALIBRATE_FLAG_OFF;
-
-        ConfigureLocatorCommand.sendCommand(mRobot, flag, newX, newY, newYaw);
-    }
-
-    public void upPressed(View v) {
+    public void startMapping(View v) {
         mRobot.drive(0, .6f);
     }
 
-    public void rightPressed(View v) {
+    public void stopMapping(View v) {
         mRobot.drive(90, .6f);
-    }
-
-    public void downPressed(View v) {
-        mRobot.drive(180, .6f);
-    }
-
-    public void leftPressed(View v) {
-        mRobot.drive(270, .6f);
-    }
-
-    public void stopPressed(View v) {
-        mRobot.stop();
     }
 
     private void turnAndDrive() {
@@ -195,7 +144,7 @@ public class LocatorActivity extends Activity {
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             public void run() {
-                if(velocityx <= 1.0f && velocityy <= 1.0f)
+                if(velocityx <= 0.2f && velocityy <= 0.2f)
                 currentAngle = randomGenerator.nextInt(359);
                 turnAndDrive();
                 startStuckHandler();
---
+++ src/com/orbotix/sample/locator/MapView.java
@@ -0,0 +1,20 @@
+package com.orbotix.sample.locator;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created: 13/12/2013
+ *
+ * @author ryanjohn
+ *
+ */
+public class MapView extends View {
+
+    public MapView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+}
