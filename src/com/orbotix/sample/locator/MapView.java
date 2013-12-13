86ecd8a61b6fda7e6dbdb8b9b35d6e49990d52c5
Robert Hewitt <robert@kotikan.com>
2013-12-13 12:55:52 +0000

    api14 required for canvas getHeight and getWidth call. implemented a basic drawing mechanic which will draw the points

--- src/com/orbotix/sample/locator/MapView.java
+++ src/com/orbotix/sample/locator/MapView.java
@@ -1,6 +1,9 @@
 package com.orbotix.sample.locator;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 import com.orbotix.sample.locator.mapping.CollisionLocatorData;
@@ -12,16 +15,91 @@ import java.util.List;
  * Created: 13/12/2013
  *
  * @author ryanjohn
- *
  */
-public class MapView extends View implements LocationViewer{
+public class MapView extends View implements LocationViewer {
+
+    private final Paint paint = new Paint();
+    private List<CollisionLocatorData> list;
 
     public MapView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        paint.setColor(Color.BLACK);
     }
 
     @Override
     public void showFor(List<CollisionLocatorData> dataList) {
+        list = dataList;
+        invalidate();
+    }
 
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        if (list == null || list.size() == 0) {
+            return;
+        }
+
+        final BoundingBoxInfo boxInfo = getBoundsOfPoints(list);
+
+        final int maxXScreenSize = canvas.getMaximumBitmapWidth();
+        final int maxYScreenSize = canvas.getMaximumBitmapHeight();
+
+        final float xRatio = ((float) maxXScreenSize) / boxInfo.size.x;
+        final float yRatio = ((float) maxYScreenSize) / boxInfo.size.y;
+
+        for (CollisionLocatorData item : list) {
+            float offsetPositionX = item.locatorData.getPositionX() + boxInfo.leftEdge;
+            float offsetPositionY = item.locatorData.getPositionY() + boxInfo.topEdge;
+
+            offsetPositionX *= xRatio;
+            offsetPositionY *= yRatio;
+
+            canvas.drawPoint(offsetPositionX, offsetPositionY, paint);
+        }
+    }
+
+    private BoundingBoxInfo getBoundsOfPoints(List<CollisionLocatorData> points) {
+        float maxX = 0f, maxY = 0f, minX = 0f, minY = 0f;
+        for (CollisionLocatorData item : points) {
+            final float positionX = item.locatorData.getPositionX();
+            final float positionY = item.locatorData.getPositionY();
+            if (positionX > maxX) {
+                maxX = positionX;
+            }
+            if (positionY > maxY) {
+                maxY = positionY;
+            }
+            if (positionX < minX) {
+                minX = positionX;
+            }
+            if (positionY < minY) {
+                minY = positionY;
+            }
+        }
+        final FloatPoint boxSize = new FloatPoint(maxX - minX, maxY - minY);
+        return new BoundingBoxInfo(boxSize, minX, minY);
+    }
+
+    private class FloatPoint {
+        final float x;
+        final float y;
+
+        private FloatPoint(float x, float y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    private class BoundingBoxInfo {
+        final FloatPoint size;
+        final float leftEdge;
+        final float topEdge;
+
+        private BoundingBoxInfo(FloatPoint size, float leftEdge, float topEdge) {
+            this.size = size;
+            this.leftEdge = leftEdge;
+            this.topEdge = topEdge;
+        }
     }
 }
